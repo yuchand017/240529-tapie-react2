8de6d017e54050fe3c2523f8623a8117f56a5ae3
Yuchan Han <serltretu24@gmail.com>
2024-06-01 18:16:35 +0900

    style: rename InputComponent, update Input, Label style

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,18 +1,28 @@
 import styled from "styled-components";
 
+const Form = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-sizing: border-box;
+    padding: 46px 16px;
+    gap: 12px;
+`
+
+const InputComponent = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
 const Label = styled.label`
     font-weight: 500;
-    font-size: 12px;
-    display: block;
-    margin-bottom: 8px;
-`;
+`
 
 const Input = styled.input`
-    font-weight: 500;
-    font-size: 12px;
+    height: 32px;
+    margin-top: 4px;
+    border:solid 1px #686868;
     border-radius: 4px;
-    border: 1px solid #000;
-    width: 100%;
-`;
+`
 
-export { Label, Input };
+export { Form, InputComponent, Label, Input };

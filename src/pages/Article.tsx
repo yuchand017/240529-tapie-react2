25ef494ad1273275bfa467aaf941fe3e570c8a86
Yuchan Han <serltretu24@gmail.com>
2024-06-03 09:33:51 +0900

    feat: add article page

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,39 +1,73 @@
+import { useEffect, useState } from "react"
+import {useNavigate, useParams} from "react-router-dom";
 import Header from "../components/Header"
-import { styled } from 'styled-components'
+import {styled} from 'styled-components'
+import {useCredential} from "../store";
 
 const ArticleView = styled.div`
     display: flex;
     flex-direction: column;
     padding: 16px;
-    
+
     h3 {
         font-size: 18px;
     }
-    
-    p:first-child {
+
+    span {
         font-size: 14px;
         color: #686868;
-        margin-bottom: 16px;
     }
-    
+
     p {
+        margin-top: 16px;
         font-size: 16px;
     }
-    
+
 ;
 `
 
 function Article() {
-    return (
-        <>
-            <Header />
-            <ArticleView>
-                <h3>ㅁㄴㅇㄹ</h3>
-                <p>A</p>
-                <p>안녕하세요 ㅐㄴ얌리노ㅕ할</p>
-            </ArticleView>
-        </>
-    )
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const token = useCredential(state => state.token)
+    const [articleData, setArticleData] = useState<{
+        id: number,
+        title: string,
+        content: string,
+        writerUsername: string,
+    }>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!token) {
+                alert("로그인이 필요합니다.")
+                navigate("/")
+                return
+            }
+            const response = await fetch(
+                'https://board.etty.dev/board/ㅁㄴㅇㄹ/posts/' + id,
+                {
+                    headers: {'Authorization': 'Bearer ' + token}
+                }
+            )
+            const responseData = await response.json();
+            setArticleData(responseData.data);
+        };
+
+        fetchData()
+    }, [])
+    if (token !== null) {
+        return (
+            <>
+                <Header />
+                <ArticleView>
+                    <h3>{articleData.title}</h3>
+                    <span>{articleData.writerUsername}</span>
+                    <p>{articleData.content}</p>
+                </ArticleView>
+            </>
+        )
+    }
 }
 
 export default Article
